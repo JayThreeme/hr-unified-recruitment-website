f8de75a6a7ba2d495a986446f65628ede2f96670
Jimmy <102960707+JayThreeme@users.noreply.github.com>
2025-01-31 03:59:45 +0000

    added calendar library and component

--- src/Pages/Admin/AdminCalendar.jsx
+++ src/Pages/Admin/AdminCalendar.jsx
@@ -1,47 +1,36 @@
-
+import { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import eventsinfo from "../../Information/EventsInfo";
+const localizer = momentLocalizer(moment);
 
 const AdminCalendar = () => {
+  const [events, setEvents] = useState([eventsinfo]);
+
   return (
     <>
-          <div className="">
-            <div className="d-flex gap-3 justify-content-start align-items-center">
-              <h5 className="m-0">Calendar</h5>
-              <ul className="nav">
-                <li className="nav-item">
-                  <a className="nav-link" aria-current="page" href="#">
-                    All
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    New
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Interview
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Screening
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="#">
-                    Hired
-                  </a>
-                </li>
-              </ul>
-            </div>
-    
-            {/* table */}
-            <div className="card p-4 m-0">
-              
-            </div>
-          </div>
-        </>
-  )
-}
+      <div className="">
+        <div className="d-flex gap-3 justify-content-start align-items-center">
+          <h5 className="m-0">Calendar</h5>
+          
+        </div>
 
-export default AdminCalendar
+        {/* table */}
+        <div className="card p-4 m-0">
+          <div>
+            <Calendar
+              localizer={localizer}
+              events={events}
+              startAccessor="start"
+              endAccessor="end"
+              style={{width: "100%", height: "600px"}}
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AdminCalendar;
